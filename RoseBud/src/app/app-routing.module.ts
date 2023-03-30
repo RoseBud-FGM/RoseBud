811a580f5898d7bd8d03e165626ee393572dcbc9
gSayak <gsayak22@gmail.com>
2023-03-30 21:41:43 +0530

    pakhi paka papaya khay

--- RoseBud/src/app/app-routing.module.ts
+++ RoseBud/src/app/app-routing.module.ts
@@ -5,8 +5,14 @@ import { ReportComponent } from './report/report.component'
 import { HeaderComponent } from './header/header.component';
 import { CounsellingComponent } from './counselling/counselling.component';
 import { ReviewComponent } from './review/review.component';
+import { TrackingComponent } from './tracking/tracking.component';
 
-const routes: Routes = [{path:'', component:LandingComponent}];
+const routes: Routes = [{path:'', component:LandingComponent},
+{path:'report', component: ReportComponent},
+{path:'counselling', component: CounsellingComponent},
+{path:'review', component: ReviewComponent},
+{path:'tracking', component: TrackingComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
